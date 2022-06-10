e0b623c6af825015d59ab4020c675b16a100ed93
Vinils76 <antoniovinicius7670@gmail.com>
2022-06-10 09:48:00 -0300

    Herança e Classe abstrata

--- index.php
+++ index.php
@@ -4,27 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 1</title>
+    <title>Exemplo 5</title>
 </head>
 <body>
-    <h1>PHP POO</h1>
+<h1>PHP POO - Exemplo 5</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Criação de classe</li>
-        <li>Importação de arquivo de classe</li>
-        <li>Criação de objetos</li>
-    </ul>
+        <li>Herança</li>
+        <li>Classe Abstrata</li>
 
 <?php
 // Importando a classe
 require_once "src/Cliente.php";
+require_once "src/PessoaFisica.php";
+require_once "src/PessoaJuridica.php";
 
 // Criação dos objetos
-$clienteA = new Cliente;
-$clienteB = new Cliente;
+$clientePF = new PessoaFisica;
+$clientePF->setNome('Antonio');
+$clientePF->setEmail('antonio@gmail');
+$clientePF->setSenha('abc123');
+$clientePF->setCpf('123.456.789-00');
+$clientePF->setIdade(20);
+
+$clientePJ = new PessoaJuridica;
+$clientePJ->setNome('Antonio');
+$clientePJ->setEmail('antonio@gmail.com');
+$clientePJ->setSenha('124556');
+$clientePJ->setCnpj('05.055.045/0001-56');
+$clientePJ->setAnoFundacao(1950);
+$clientePJ->setNomeFantasia('infotec');
 ?>
 
-<pre> <?=var_dump($clienteA, $clienteB)?> </pre>
+<pre> <?=var_dump($clientePF)?> </pre>
+<pre><?=var_dump($clientePJ)?></pre>
 </body>
 </html>
--- src/Cliente.php
+++ src/Cliente.php
@@ -1,9 +1,32 @@
 <?php
-class Cliente {
-    // Propriedades/Atributos
+/* Classes abstratas não podem ser instanciadas, ou seja, não é possível criar um objeto/variável a partir dessa classe */
+abstract class Cliente {
+    private string $nome;
+    private string $email;
+    private string $senha;
+    
+    /* Métodos getters e setters */
+    public function getNome():string {
+        return $this->nome;
+    }
 
-    public string $nome;
-    public string $email;
-    public string $senha;
-    public array $telefones;
+    public function setNome(string $nome){
+        $this->nome = $nome;
+    }
+
+    public function getEmail():string {
+        return $this->email;
+    }
+
+    public function setEmail(string $email){
+        $this->email = $email;
+    }
+
+    public function getSenha():string {
+        return $this->senha;
+    }
+
+    public function setSenha(string $senha){
+        $this->senha = password_hash($senha, PASSWORD_DEFAULT);
+    }
 }
